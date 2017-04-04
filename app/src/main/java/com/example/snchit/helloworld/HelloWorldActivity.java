8750af6b90f78666983544482310366fa528f921
Sanchit Kumar Singh <sanchitkumarsingh@gmail.com>
2017-04-04 17:24:12 +0530

    Hello World

--- app/src/main/java/com/example/snchit/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/example/snchit/helloworld/HelloWorldActivity.java
@@ -1,17 +1,49 @@
 package com.example.snchit.helloworld;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
     String msg = "Android : ";
+    Button nextButton;
+    TextView displayTextView;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        displayTextView = (TextView) findViewById(R.id.textView);
+        nextButton = (Button) findViewById(R.id.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (displayTextView.getText() == "HELLO WORLD!!"){
+                    displayTextView.setText("Mission Accomplished. Move to other task.");
+                }else{
+                    displayTextView.setText("HELLO WORLD!!");
+                }
+                Log.d(msg, "Button clicked");
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -21,26 +53,59 @@ public class HelloWorldActivity extends AppCompatActivity {
     }
 
     @Override
-    protected  void  onStart(){
+    protected void onStart() {
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
         Log.d(msg, "App Started");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "HelloWorld Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.snchit.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     @Override
-    protected  void  onPause(){
+    protected void onPause() {
         super.onPause();
         Log.d(msg, "App Paused");
     }
 
     @Override
-    protected  void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         Log.d(msg, "App Destroyed");
     }
 
     @Override
-    protected  void onStop(){
+    protected void onStop() {
         super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "HelloWorld Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.snchit.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
         Log.d(msg, "App Stopped");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
     }
+
 }
